6b33295254952b955f3f81de874930060d67279b
Aaron Amato <aamato@pnptc.com>
2020-04-16 12:55:19 +0200

    Enabled dhango user admin for user profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,18 +4,21 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
+
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
+
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
         user.set_password(password)
-        user.save(using+self.db)
+        user.save(using=self.db)
 
         return user
+        
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
