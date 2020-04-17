eef078da3a734eb8ddfbf3731aa26e6ce8fc7c5a
Aaron Amato <aamato@pnptc.com>
2020-04-17 12:36:05 +0200

    Added permission

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
-from profiles_api import models 
+
+from profiles_api import models
+
 
 class HelloSerializer(serializers.Serializer):
-    """Serializes a name field for testion our API view"""
+    """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
 
+
 class UserProfileSerializer(serializers.ModelSerializer):
-    '''Serializes a user profile object'''
+    """Serializes a user profile object"""
+
     class Meta:
         model = models.UserProfile
         fields = ('id', 'email', 'name', 'password')
@@ -18,18 +22,30 @@ class UserProfileSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        '''Create and return a new user'''
+        """Create and return a new user"""
         user = models.UserProfile.objects.create_user(
             email=validated_data['email'],
-            name=validated_data_data['name'],
+            name=validated_data['name'],
             password=validated_data['password']
         )
 
         return user
 
     def update(self, instance, validated_data):
-        '''Handle updating user account'''
+        """Handle updating user account"""
         if 'password' in validated_data:
             password = validated_data.pop('password')
             instance.set_password(password)
+
         return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
+
+
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,11 +4,13 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
-
 
 
 class HelloApiView(APIView):
@@ -44,7 +46,7 @@ class HelloApiView(APIView):
         """Handle updating an object"""
         return Response({'method': 'PUT'})
 
-    def path(self, request, pk=None):
+    def patch(self, request, pk=None):
         """Handle a partial update of an object"""
         return Response({'method': 'PATCH'})
 
@@ -52,14 +54,15 @@ class HelloApiView(APIView):
         """Delete an object"""
         return Response({'method': 'DELETE'})
 
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
-    serializer_class= serializers.HelloSerializer
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Return a hello message"""
         a_viewset = [
-            'Users actions (list, create, retrieve, update, partial_update)',
+            'Uses actions (list, create,retrieve, update, partial_update)',
             'Automatically maps to URLs using Routers',
             'Provides more functionality with less code',
         ]
@@ -80,27 +83,46 @@ class HelloViewSet(viewsets.ViewSet):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-    def retreive(self, request, pk=None):
-        """Handle getting an object by itsID"""
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
         return Response({'http_method': 'GET'})
 
     def update(self, request, pk=None):
         """Handle updating an object"""
-        return Response({'http_method': 'PUT'}) 
+        return Response({'http_method': 'PUT'})
 
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
         return Response({'http_method': 'PATCH'})
 
     def destroy(self, request, pk=None):
-        """Handle removal of an object"""
+        """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
-    '''Handle creating and updating profiles'''
-    serializer_class  = serializers.UserProfileSerializer
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    '''Handles creating, reading and updating profile feed the items'''
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus, IsAuthenticated
+    )
+    def perform_crate(self, serializer):
+        '''sets the use profile to the logged in user'''
+        serializer.save(user_profile=self.request.user)
